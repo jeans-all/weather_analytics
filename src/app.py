bb81254a61eddd5fd117d6dd73c7dde8632b65b0
Jinsol Kim <jinsolkim91@gmail.com>
2024-11-18 20:15:23 +0800

    brushing up

--- src/app.py
+++ src/app.py
@@ -2,24 +2,25 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 import plotly.express as px
+import plotly.graph_objects as go
 from datetime import datetime, timedelta
 
 # 페이지 기본 설정
 st.set_page_config(
-    page_title="날씨 모니터링 대시보드",
+    page_title="Weather Monitoring Dashboard",
     layout="wide"
 )
 
 # 제목
-st.title("실시간 날씨 모니터링 대시보드")
+st.title("Real Time Weather Monitoring Dashboard")
 
 # DB에서 최신 데이터 조회
 def get_latest_weather():
     conn = sqlite3.connect('data/weather_data.db')
     query = """
-    SELECT city, temperature, humidity, weather_description, wind_speed, timestamp
-    FROM weather_records
-    WHERE timestamp = (SELECT MAX(timestamp) FROM weather_records)
+    SELECT city, temperature, temperature_feels_like, humidity, weather_description, wind_speed, timestamp  
+    FROM weather_records  
+    WHERE (city, timestamp) IN (SELECT city, MAX(timestamp) FROM weather_records GROUP BY city)
     """
     df = pd.read_sql_query(query, conn)
     conn.close()
@@ -29,51 +30,127 @@ def get_latest_weather():
 def get_historical_data():
     conn = sqlite3.connect('data/weather_data.db')
     query = """
-    SELECT city, temperature, humidity, timestamp
+    SELECT city, temperature, temperature_feels_like, humidity, timestamp, wind_speed
     FROM weather_records
     WHERE timestamp >= datetime('now', '-1 day')
     """
     df = pd.read_sql_query(query, conn)
     conn.close()
+    # print(df)
     return df
+
+
 
 # 최신 데이터 표시
 latest_data = get_latest_weather()
 
 # 도시별 현재 날씨 카드 표시
-st.subheader("현재 날씨 현황")
+st.subheader("Current Weather Status", anchor="Section-1")
 cols = st.columns(len(latest_data))
 for idx, (_, row) in enumerate(latest_data.iterrows()):
     with cols[idx]:
         st.metric(
             label=row['city'],
             value=f"{row['temperature']}°C",
-            delta=f"습도 {row['humidity']}%"
+            delta=f"Humidity {row['humidity']}%"
         )
-        st.write(f"날씨: {row['weather_description']}")
-        st.write(f"풍속: {row['wind_speed']}m/s")
+        st.write(f"Weather: {row['weather_description']}")
+        st.write(f"Wind speed: {row['wind_speed']}m/s")
+
+
+# 실제 온도 vs 체감 온도
+st.subheader("Real Temperature vs Perceived Temperature", anchor="Section-2")
+fig_feeling = go.Figure()
+fig_feeling.add_trace(go.Bar(
+    x=latest_data["city"], 
+    y=latest_data["temperature"], 
+    name="Real Temperature",
+    marker=dict(
+        color=latest_data['temperature'],
+        colorscale='sunset_r',
+        showscale=True,
+        cmin=-10,
+        cmax=10, 
+        colorbar=dict(
+            title="Temperature", 
+            len=0.8,
+            y = 0.38
+        ) 
+    )
+    )
+)
+fig_feeling.add_trace(go.Bar(
+    x=latest_data["city"], 
+    y=latest_data["temperature_feels_like"], 
+    name="Perceived Temperature", 
+    marker=dict(
+        color=latest_data['temperature_feels_like'],
+        colorscale='sunset_r',
+        cmin=-10,
+        cmax=10,
+        colorbar=dict(
+            title="Temperature_feels_like", 
+            len=0.8,
+            y = 0.38
+        ) 
+    ))
+)
+fig_feeling.update_layout(barmode="group")
+
+st.plotly_chart(fig_feeling, use_container_width=True)
 
 # 시계열 그래프
-st.subheader("지역별 기온 변화")
+st.subheader("Temperature Changes Over Time in Cities", anchor="Section-3")
 historical_data = get_historical_data()
+print(historical_data)
+# exit()
 fig = px.line(
     historical_data,
     x='timestamp',
     y='temperature',
-    color='city',
-    title='도시별 기온 변화'
+    color='city'
+    # title='Temperature Change in Cities'
 )
 st.plotly_chart(fig, use_container_width=True)
 
+
+# 습도와 풍속과의 관계
+st.subheader("Relation Between Humidity and Temperature", anchor="Section-4")
+fig_scatter = px.scatter(historical_data.sort_values('humidity'), x="humidity", y="wind_speed", color="temperature", 
+                         size="temperature", hover_data=["city"], labels={"humidity": "습도 (%)", "wind_speed": "풍속 (m/s)"})
+st.plotly_chart(fig_scatter, use_container_width=True)
+
+
 # 습도 분포 그래프
-st.subheader("현재 습도 분포")
+st.subheader("Current Humidity Distribution", anchor="Section-5")
 fig_humidity = px.bar(
     latest_data,
     x='city',
     y='humidity',
-    title='도시별 습도'
+    color = 'humidity',
+    color_continuous_scale='bluyl',
+    range_color = [0, 100],
+    title='Humidity Per City'
 )
 st.plotly_chart(fig_humidity, use_container_width=True)
 
-# 데이터 업데이트 시간 표시
-st.sidebar.write("최종 업데이트:", latest_data['timestamp'].iloc[0])
+
+# sidebar 
+st.sidebar.title("Contents")
+st.sidebar.markdown("""
+<ul>
+    <li><a href='#Section-1'> Current Weather Status </a></li>
+    <li><a href='#Section-2'> Real Temperature vs Perceived Temperature </a></li>
+    <li><a href='#Section-3'> Temperature Changes Over Time in Cities </a></li>
+    <li><a href='#Section-4'> Relation Between Humidity and Temperature </a></li>
+    <li><a href='#Section-5'> Current Humidity Distribution </a></li>
+</ul>
+""", unsafe_allow_html=True)
+
+st.sidebar.header("Information")
+st.sidebar.metric("Last updated:", latest_data['timestamp'].iloc[0])
+
+hottest = latest_data.loc[latest_data["temperature"] == latest_data["temperature"].max(), 'city'].iloc[0]
+coolest = latest_data.loc[latest_data["temperature"] == latest_data["temperature"].min(), 'city'].iloc[0]
+st.sidebar.metric("Hottest City", f'{hottest} at {latest_data["temperature"].max()}')
+st.sidebar.metric("Coldest City", f'{coolest} at {latest_data["temperature"].min()}')
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import requests
 import json
 import time
+import sqlite3
 
 from datetime import datetime
 from dotenv import load_dotenv
@@ -33,16 +34,21 @@ def get_weather_data(city):
     
     if response.status_code == 200:
         data = response.json()
-        
         # 필요한 데이터만 추출
         weather_info = {
             'city': city.split(',')[0],
             'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
             'temperature': data['main']['temp'],
+            'temperature_feels_like': data['main']['feels_like'],
             'humidity': data['main']['humidity'],
             'weather_description': data['weather'][0]['description'],
             'wind_speed': data['wind']['speed']
         }
+        
+        # data['timestamp'] = weather_info['timestamp']
+        # data['city'] = weather_info['city']
+        # print(weather_info)
+        # return (weather_info, data)
         return weather_info
     else:
         print(f"Error fetching data for {city}: {response.status_code}")
@@ -51,13 +57,22 @@ def get_weather_data(city):
 # 모든 도시의 데이터 수집
 def collect_all_cities():
     all_data = []
+    all_fullData = []
     for city in CITIES:
+        # (weather_data, fullData) = get_weather_data(city)
         weather_data = get_weather_data(city)
+        # print(weather_data)
         if weather_data:
             all_data.append(weather_data)
+            # all_fullData.append(fullData)
         time.sleep(1)  # API 호출 제한 고려
+    # print(all_data)
+    # print()
+    # print(all_fullData)
+    # return (all_data, all_fullData)
     return all_data
 
+# (weather_data, weather_fullData) = collect_all_cities()
 weather_data = collect_all_cities()
 # print(json.dumps(weather_data, indent=2))
 
@@ -66,43 +81,64 @@ weather_data = collect_all_cities()
 
 def process_weather_data(weather_data):
     # JSON 데이터를 DataFrame으로 변환
+
+    print(weather_data)
+    print()
     df = pd.DataFrame(weather_data)
     
+
     # timestamp 컬럼을 datetime 타입으로 변환
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    try:
+        df['timestamp'] = pd.to_datetime(df['timestamp'])
     
+    except:
+        print(df)
+        print(df.columns)
+        exit()
+
     # 도시명을 인덱스로 설정
     df.set_index('city', inplace=True)
     
     # 컬럼명 한글로 변경 (선택사항)
-    df.columns = ['측정시각', '기온', '습도', '날씨', '풍속']
-    
+    # print(df.columns)
+    # df.columns = ['측정시각', '기온', '체감기온', '습도', '날씨', '풍속']
     # 소수점 자릿수 조정
-    df['기온'] = df['기온'].round(1)
-    df['풍속'] = df['풍속'].round(1)
+    # df['기온'] = df['기온'].round(1)
+    # df['풍속'] = df['풍속'].round(1)
     
     return df
 
-df_weather = process_weather_data(weather_data)
-# print(df_weather)
+def process_weather_dataFull(weather_fullData):
+    df = pd.DataFrame(weather_fullData)
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df.set_index('city', inplace = True)
+    
+    return df
 
-import sqlite3
+
+df_weather = process_weather_data(weather_data)
+# df_weather_fullData = process_weather_dataFull(weather_fullData)
+# print(df_weather_fullData['main'])
+
 
 def create_database():
     conn = sqlite3.connect('data/weather_data.db')
     cursor = conn.cursor()
-
+    
     query = '''
         CREATE TABLE IF NOT EXISTS weather_records (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             city TEXT NOT NULL,
             timestamp DATETIME NOT NULL,
             temperature FLOAT,
+            temperature_feels_like, FLOAT,
             humidity INTEGER,
             weather_description TEXT,
             wind_speed FLOAT
         )
     '''
+
+    
 
     cursor.execute(query)
 
@@ -115,7 +151,8 @@ def save_to_database(df):
     # DataFrame을 SQLite DB에 저장
     # 한글 컬럼명을 다시 영어로 변환
     df_to_save = df.reset_index()
-    df_to_save.columns = ['city', 'timestamp', 'temperature', 'humidity',
+    # print(df_to_save)
+    df_to_save.columns = ['city', 'timestamp', 'temperature', 'temperature_feels_like', 'humidity',
                          'weather_description', 'wind_speed']
 
     # DB에 데이터 저장
@@ -127,7 +164,7 @@ def save_to_database(df):
 
 # 데이터베이스 저장 실행
 save_to_database(df_weather)
-
+# save_to_databaseFull(df_weather_fullData)
 # 저장된 데이터 확인
 def check_saved_data():
     conn = sqlite3.connect('data/weather_data.db')
